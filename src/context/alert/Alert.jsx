44d93af2a98e436df731544ff6726e89a83ed5b8
Emran <emranitmbstu@gmail.com>
2023-09-15 00:29:54 +0600

    correction

--- src/context/alert/Alert.jsx
+++ src/context/alert/Alert.jsx
@@ -5,9 +5,11 @@ function Alert() {
     const { alert } = useContext(AlertContext);
     return alert !== null && alert.type === 'error' &&
         (
-            <div className="alert alert-error">
-                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 20 20"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                <span>{alert.massag}</span>
+            <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2">
+                <div className="alert alert-error">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 20 20"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                    <span>{alert.massag}</span>
+                </div>
             </div>
         )
 }
